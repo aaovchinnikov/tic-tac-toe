3e8926597cd2bc06ebf678ce388c9f512709b50d
Alex Ovchinnikov <SanSeyp6@gmail.com>
2020-09-16 22:34:53 +0300

    Added Field to ConsoleTicTacToe

--- src/main/java/com/sansey/tictactoe/ConsoleTicTacToe.java
+++ src/main/java/com/sansey/tictactoe/ConsoleTicTacToe.java
@@ -11,18 +11,24 @@ public final class ConsoleTicTacToe implements Game {
    * {@link PrintStream} where to print console output.
    */
   private final PrintStream out;
+  /**
+   * Initial game {@link Field}.
+   */
+  private final Field field;
 
   /**
    * Main constructor.
    * @param stream - {@link PrintStream} where to print console output
+   * @param f - {@link Field} to play on
    */
-  public ConsoleTicTacToe(final PrintStream stream) {
+  public ConsoleTicTacToe(final PrintStream stream, final Field f) {
     this.out = stream;
+    this.field = f;
   }
-
 
   @Override
   public void start() {
     this.out.println("Tic-tac-toe game started!");
+    this.field.printTo(out);
   }
 }
--- src/test/java/com/sansey/tictactoe/ConsoleTicTacToeTest.java
+++ src/test/java/com/sansey/tictactoe/ConsoleTicTacToeTest.java
@@ -14,10 +14,16 @@ class ConsoleTicTacToeTest {
   void testStart() {
     ByteArrayOutputStream baos = new ByteArrayOutputStream();
     new ConsoleTicTacToe(
-      new PrintStream(baos)
+      new PrintStream(baos),
+      new Field() {
+        @Override
+        public void printTo(PrintStream out) {
+          out.println("Field");
+        }
+      }
     ).start();
     assertEquals(
-        "Tic-tac-toe game started!" + System.lineSeparator(),
+        "Tic-tac-toe game started!" + System.lineSeparator() + "Field" + System.lineSeparator(),
         baos.toString(StandardCharsets.UTF_8)
     );
   }
