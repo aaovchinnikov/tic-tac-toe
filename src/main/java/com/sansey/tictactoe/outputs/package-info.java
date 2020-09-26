c177464efc96009777774359934a0d4c382c9533
Alex Ovchinnikov <SanSeyp6@gmail.com>
2020-09-26 13:19:06 +0300

    Added packages for implementations

---
+++ src/main/java/com/sansey/tictactoe/outputs/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Package that holds implementations of
+ * {@link com.sansey.tictactoe.Output} interface.
+ * @author Alexander Ovchinnikov
+ */
+package com.sansey.tictactoe.outputs;
---
+++ src/main/java/com/sansey/tictactoe/turns/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Package that holds implementations of
+ * {@link com.sansey.tictactoe.Turn} interface.
+ * @author Alexander Ovchinnikov
+ */
+package com.sansey.tictactoe.turns;
