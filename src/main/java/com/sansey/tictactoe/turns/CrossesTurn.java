32e72a5643a4e51dd65a613b6fe7ce42daf10fa3
Alex Ovchinnikov <SanSeyp6@gmail.com>
2020-11-26 23:35:58 +0300

    Removed input specific Turn implementations

--- src/main/java/com/sansey/tictactoe/turns/CrossesTurn.java
+++ src/main/java/com/sansey/tictactoe/turns/CrossesTurn.java
@@ -1,8 +1,16 @@
 package com.sansey.tictactoe.turns;
 
+import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
 import com.sansey.tictactoe.Field;
 import com.sansey.tictactoe.Turn;
 import com.sansey.tictactoe.TurnResult;
+import com.sansey.tictactoe.ints.NaturalInt;
+import com.sansey.tictactoe.results.ConsoleCrossesTurnResult;
+import com.sansey.tictactoe.values.SimpleIntValueAt;
+import com.sansey.tictactoe.values.ValidatedFieldValue;
 
 public final class CrossesTurn implements Turn{
   /**
@@ -21,6 +29,37 @@ public final class CrossesTurn implements Turn{
   @Override
   public TurnResult result() {
     // TODO Auto-generated method stub
+    /*
+    // FIXME get rid of knowledge of field size.
+    final int size = 3;
+    while (true) {
+      this.out.print("X's turn. Type cell coordinates - row and "
+          + "column (e.g.: 0 2) - and press \'Enter\'-key: ");
+      try {
+        final Scanner input = new Scanner(this.scanner.nextLine());
+        final int row = input.nextInt();
+        final int column = input.nextInt();
+        this.out.println("Row is " + row);
+        this.out.println("Columnt is " + column);
+        final Field fld = this.field.withValueAt(
+            new ValidatedFieldValue(
+                new SimpleIntValueAt(1, row, column),
+                new NaturalInt(size)
+            )
+        );
+        return new ConsoleCrossesTurnResult(code, fld);
+      } catch (InputMismatchException ime) {
+        this.out.println("Input is not an integer coordinates.");
+      } catch (NoSuchElementException nse) {
+        this.out.println(
+            "Input should contain two coordinates but only one entered."
+        );
+      } catch (Exception e) {
+        this.out.println(e.getMessage());
+      }
+     * 
+     * 
+     */
     return null;
   }
 
